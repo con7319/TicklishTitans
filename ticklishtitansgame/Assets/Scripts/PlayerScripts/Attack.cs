df3de8e7270dabbd37d60d3e0a449e3e4fd4a57f
con7319 <con7319@gmail.com>
2024-01-27 20:11:31 +0000

    jokes ? over 199 of them!
    added csv and jokes stuff

--- ticklishtitansgame/Assets/Scripts/PlayerScripts/Attack.cs
+++ ticklishtitansgame/Assets/Scripts/PlayerScripts/Attack.cs
@@ -1,9 +1,34 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
-{
+{   
+    ArrayList jokesListArray = new ArrayList();
+    void Start()
+    {
+        
+        string filePath = @"Assets\csv\jokeslist.csv";
+        
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                        jokesListArray.Add(line); // Adding each line to the ArrayList
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
     private void Update()
     {
         if(GameManager.Instance.isAttacking && Input.GetMouseButtonDown(0))
@@ -27,8 +52,19 @@ public class Attack : MonoBehaviour
 
     private void Joke()
     {
-        Debug.Log("Insert joke here");
+        Debug.Log("choosing joke");
+        
+        int randomInt = UnityEngine.Random.Range(0, jokesListArray.Count);
 
+        if (randomInt >= 0 && randomInt < jokesListArray.Count)
+            {
+            var line = jokesListArray[randomInt]; 
+            
+            Debug.Log("joke chosen:");
+            Debug.Log(line);
+                
+            }
+        
         //Add animation code
         //Make this do something
     }
