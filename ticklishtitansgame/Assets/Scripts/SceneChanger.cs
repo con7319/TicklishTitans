ec0300fe21fed43052214cf4f13688b7480f74ef
StNick20 <150795906+StNick20@users.noreply.github.com>
2024-01-27 13:30:06 +0000

    Update SceneChanger.cs

--- ticklishtitansgame/Assets/Scripts/SceneChanger.cs
+++ ticklishtitansgame/Assets/Scripts/SceneChanger.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public GameObject smiley;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        smiley = GameObject.Find("Smiley");
+        smiley.SetActive(false);
     }
 
     public void Play()
     {
+        smiley.SetActive(true);
         SceneManager.LoadScene("SampleScene");
     }
 
