b24b3fe7a42c880488292ad5668b43c084b1288d
con7319 <con7319@gmail.com>
2024-01-27 14:26:42 +0000

    mad additions
    destructable trees and cleanup fragment manager script implemented

---
+++ ticklishtitansgame/Assets/FragmentManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentManager : MonoBehaviour
+{
+    private List<GameObject> fragments = new List<GameObject>();
+    private float timeToDestroy = 5f;
+
+    void Update()
+    {
+        GameObject[] allFragments = GameObject.FindGameObjectsWithTag("Fragment");
+        foreach (GameObject fragment in allFragments)
+        {
+            if (!fragments.Contains(fragment))
+            {
+                // New fragment found, add to list and start coroutine to destroy it
+                fragments.Add(fragment);
+                StartCoroutine(DestroyAfterTime(fragment, timeToDestroy));
+            }
+        }
+    }
+
+    private IEnumerator DestroyAfterTime(GameObject fragment, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Before destroying, check if the fragment still exists
+        if (fragment != null)
+        {
+            fragments.Remove(fragment);
+            Destroy(fragment);
+        }
+    }
+}
