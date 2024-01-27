9edc86e4198d027d8bf350ef8350f1af959717a3
con7319 <con7319@gmail.com>
2024-01-27 14:29:03 +0000

    Revert "Merge branch 'main' into map-arena1"
    This reverts commit bd40cb76c56a7124495aa6fa17b9c039f400904e, reversing
    changes made to b24b3fe7a42c880488292ad5668b43c084b1288d.

--- ticklishtitansgame/Assets/Scripts/ButtonController.cs
+++ ticklishtitansgame/Assets/Scripts/ButtonController.cs
@@ -1,37 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using UnityEngine.EventSystems;
-using Unity.VisualScripting;
 
-public class ButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ButtonController : MonoBehaviour
 {
-
-    public Button button;
-    public TMP_Text text;
-
     // Start is called before the first frame update
     void Start()
     {
-        button = GetComponent<Button>();
-        text = GetComponentInChildren<TMP_Text>();
+        
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    // Update is called once per frame
+    void Update()
     {
-        text.color = SetRandomColor();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        text.color = Color.white;
-    }
-
-    public Color SetRandomColor()
-    {
-        Color color = new Color(Random.value, Random.value, Random.value, 1.0f);
-        return color;
+        
     }
 }
